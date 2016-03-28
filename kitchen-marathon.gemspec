0af38ae81aed6a40c018c0fe0dc3d3cf2adeeb33
Anthony Spring <tony@porkchopsandpaintchips.com>
2016-03-28 16:08:35 -0400

    Initial commit of create and delete

---
+++ kitchen-marathon.gemspec
@@ -0,0 +1,27 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'kitchen/driver/marathon_version'
+
+Gem::Specification.new do |spec|
+  spec.name          = 'kitchen-marathon'
+  spec.version       = Kitchen::Driver::MARATHON_VERSION
+  spec.authors       = ['Anthony Spring']
+  spec.email         = ['tony@porkchopsandpaintchips.com']
+  spec.description   = %q{A Test Kitchen Driver for Marathon}
+  spec.summary       = spec.description
+  spec.homepage      = 'http://github.com/yieldbot/kitchen-marathon'
+  spec.license       = 'Apache 2.0'
+
+  spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
+  spec.executables   = []
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ['lib']
+
+  spec.add_dependency 'test-kitchen',       '>= 1.0.0'
+  spec.add_dependency 'marathon-api',       '~> 1.3.2'
+
+  spec.add_development_dependency 'bundler'
+  spec.add_development_dependency 'rake'
+  spec.add_development_dependency 'rspec'
+end
---
+++ lib/kitchen/driver/marathon.rb
@@ -0,0 +1,147 @@
+# -*- encoding: utf-8 -*-
+#
+# Author:: Anthony Spring (<tony@porkchopsandpaintchips.com>)
+#
+# Copyright (C) 2016, Anthony Spring
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'json'
+require 'kitchen'
+require 'marathon'
+require 'retryable'
+
+#require_relative ""
+
+module Kitchen
+
+  module Driver
+
+    # Marathon driver for Kitchen.
+    #
+    # @author Anthony Spring <tony@porkchopsandpaintchips.com>
+    class Marathon < Kitchen::Driver::SSHBase
+
+      default_config :app_prefix, 'kitchen-'
+      default_config :app_template
+      expand_path_for :app_template
+
+      # Marathon HTTP Configuration
+
+      default_config :host, 'http://localhost:8080'
+      default_config :password
+      default_config :username
+      default_config :verify_ssl, false
+
+      # Marathon Proxy Configuration
+
+      default_config :http_proxyaddr
+      default_config :http_proxyport
+      default_config :http_proxyuser
+      default_config :http_proxypass
+
+      # Creates a new Driver object using the provided configuration data
+      # which will be merged with any default configuration.
+      #
+      # @param config [Hash] provided driver configuration
+      def initialize(config = {})
+        # Let our parent do its work
+        super(config)
+
+        # Initialize marathon
+        initialize_marathon
+      end
+
+      def create(state)
+        return if state[:app_id]
+
+        # Generate the application configuration
+        app_config = generate_app_config
+
+        # Create the app
+        state[:app_id] = create_app(app_config)
+      end
+
+      def destroy(state)
+        return if state[:app_id].nil?
+
+        begin
+          ::Marathon::App.delete(state[:app_id])
+        rescue ::Marathon::Error::NotFoundError => e
+        end 
+
+        state.delete(:app_id)
+      end
+
+      protected
+
+      def create_app(config)
+        begin
+          # Attempt to get the app from the server
+          app = ::Marathon::App.get(config['id'])
+          puts app.change!(config, true)
+        rescue ::Marathon::Error::NotFoundError => e
+          # If the app isnt found, this is a new app so create the app
+          puts ::Marathon::App.create(config)
+        end
+
+        config['id']
+      end
+
+      def create_app_id
+        "#{config[:app_prefix]}#{SecureRandom.hex}"
+      end
+
+      def generate_app_config
+        # Generate the necessary app config
+        base_config = {}
+        base_config['id']         = create_app_id
+        base_config['instances']  = 1
+
+        # Bring in the user defined JSON template
+        user_config = if File.file?(config[:app_template])
+          JSON.parse(IO.read(config[:app_template]))
+        else
+          {}
+        end
+        
+        user_config.merge(base_config)
+      end
+
+      def initialize_marathon
+        # Initialize Marathon based off of configuration data
+        marathon = {}
+
+        # Basic HTTP Information
+        marathon[:username] = config[:username]
+        marathon[:password] = config[:password]
+
+        # Basic SSL information
+        marathon[:verify]   = config[:verify_ssl]
+
+        # Basic Proxy information
+        marathon[:http_proxyaddr] = config[:http_proxyaddr]
+        marathon[:http_proxyport] = config[:http_proxyport]
+        marathon[:http_proxyuser] = config[:http_proxyuser]
+        marathon[:http_proxypass] = config[:http_proxypass]
+
+        # Set the Marathon credentials if given
+        ::Marathon.options = marathon 
+
+        # Set the Marathon URL
+        ::Marathon.url = config[:host]
+      end
+    end
+  end
+end
+

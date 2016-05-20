7ef042fde5c526727be8c7aebf519764e9a4badc
Anthony Spring <aspring@yieldbot.com>
2016-05-20 09:35:00 -0400

    Version bump

--- lib/kitchen/driver/marathon_version.rb
+++ lib/kitchen/driver/marathon_version.rb
@@ -23,8 +23,8 @@ module Kitchen
     # This defines the version of the gem
     module Version
       MAJOR = 0
-      MINOR = 0
-      PATCH = 4
+      MINOR = 1
+      PATCH = 0
       BUILD = ''.freeze
 
       STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').chomp('.')
